39177653c8452840e7bc64ed685ae8774be48dc5
itachiluv <maniveradis@gmail.com>
2024-04-15 19:21:26 +0530

    login_required

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from .models import *
 from .forms import *
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 import json
 from django.http import JsonResponse
 # Create your views here.
